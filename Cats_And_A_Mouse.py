da825fa392d98401174bcfa10b21880d8757b222
asayda01 <ahmetcemsaydam@gmail.com>
2023-01-02 11:36:25 +0000

    added some challenges and their solutions
    added some challenges and their solutions

---
+++ Cats_And_A_Mouse.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the catAndMouse function below.
+
+def catAndMouse(x, y, z):
+    
+    Cat_A = abs(z - x)
+    Cat_B = abs(z - y)
+
+    if Cat_A < Cat_B:
+        return 'Cat A'
+    elif Cat_B < Cat_A:
+        return 'Cat B'
+    else:
+        return 'Mouse C'
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        xyz = input().split()
+
+        x = int(xyz[0])
+
+        y = int(xyz[1])
+
+        z = int(xyz[2])
+
+        result = catAndMouse(x, y, z)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
