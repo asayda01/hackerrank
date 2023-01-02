f5ab5c67fb3f31a82f98752540bf4b82314c2a0f
Cem Saydam <ahmetcemsaydam@gmail.com>
2023-01-02 10:51:43 +0000

    created Bon_Appetit.py
    created Bon_Appetit.py in Python for the Hackerrank Challenge

---
+++ Bon_Appetit.py
@@ -0,0 +1,36 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    bill1 = bill[0:k]+bill[k+1:]
+    if sum(bill1)/2 == b:
+        print('Bon Appetit')
+    else:
+        print(int(b - sum(bill1)/2))
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
