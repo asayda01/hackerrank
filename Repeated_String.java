d528b1298fc98054fc3979ba82efdd3f8198571d
Cem Saydam <ahmetcemsaydam@gmail.com>
2023-01-02 10:41:45 +0000

    implemented the function
    implemented the function

--- Repeated_String.java
+++ Repeated_String.java
@@ -79,3 +79,71 @@ Because all of the first
 letters of the infinite string are a, we return . 
 
 */
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'repeatedString' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. LONG_INTEGER n
+     */
+
+
+    public static long repeatedString(String s, long n) {
+
+
+
+         int freq=0;
+
+         for (int i = 0; i <s.length(); i++) {
+             if (s.charAt(i)=='a') {
+                 freq++;
+             }
+
+         }
+
+           long count=freq *(n/s.length());
+                     long reminder=n%s.length();
+          for (int i = 0; i <reminder; i++) {
+             if (s.charAt(i)=='a') {
+                 count++;
+             }
+
+         }
+         return count;
+}
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        long n = Long.parseLong(bufferedReader.readLine().trim());
+
+        long result = Result.repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
