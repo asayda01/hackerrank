e192281569da1f166c5a4d3ef856be2b08d0843f
asayda01 <ahmetcemsaydam@gmail.com>
2023-01-02 13:31:42 +0000

    added some challenges and their solutions
    added some challenges and their solutions

---
+++ The_Hurdle_Race.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'hurdleRace' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY height
+ */
+
+function hurdleRace(k, height) {
+    const tallest_hurdle = Math.max(...height);
+    if (k >= tallest_hurdle) {
+        return 0;
+    };
+    return tallest_hurdle - k;
+};
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const k = parseInt(firstMultipleInput[1], 10);
+
+    const height = readLine().replace(/\s+$/g, '').split(' ').map(heightTemp => parseInt(heightTemp, 10));
+
+    const result = hurdleRace(k, height);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
