73ab638d5ba72e6e3217c49d60e0eb55683907e4
asayda01 <ahmetcemsaydam@gmail.com>
2023-01-23 13:26:07 +0000

    added the Strange Advertising challenge
    added the Strange Advertising challenge`s solution in Java.

---
+++ Strange_Advertising.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'viralAdvertising' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER n as parameter.
+     */
+
+    public static int viralAdvertising(int n) {
+        
+        int people = 5, likes = 0;
+        
+        for(int i = 1; i <= n; i++){
+            likes += (int) Math.floor(people/2);
+            people = (int) Math.floor(people/2) * 3;
+        }
+
+        return likes;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.viralAdvertising(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
