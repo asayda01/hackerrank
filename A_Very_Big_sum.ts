9c9770800d0c32ad1a38db1d4e01cef7509657ce
asayda01 <ahmetcemsaydam@gmail.com>
2023-02-06 16:49:16 +0000

    added a solution to the challenge in TypeScript .
    added a solution to the challenge A Very Big Sum in TypeScript .

---
+++ A_Very_Big_sum.ts
@@ -0,0 +1,50 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'aVeryBigSum' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts LONG_INTEGER_ARRAY ar as parameter.
+ */
+
+function aVeryBigSum(ar: number[]): number {
+    return ar.reduce( (a:number,b:number) => a+b,0);
+
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const arCount: number = parseInt(readLine().trim(), 10);
+
+    const ar: number[] = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result: number = aVeryBigSum(ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
