7c84159f49174d55f6564fde89c24673fa28868d
asayda01 <ahmetcemsaydam@gmail.com>
2023-02-11 11:17:56 +0000

    added a solution to the Mini Max Sum Challenge
    added a solution to the Mini Max Sum Challenge in JavaScript .

---
+++ Mini_Max_Sum.js
@@ -0,0 +1,42 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    const newArr = arr.sort(function(a, b){return a - b});
+    let minSum   = newArr[0]+newArr[1]+newArr[2]+newArr[3];
+    let maxSum   = newArr[1]+newArr[2]+newArr[3]+newArr[4];
+    console.log(`${minSum} ${maxSum}`);
+
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
