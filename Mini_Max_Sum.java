e4cd308a9521b73b5abb3cfb798f47e6ba464614
asayda01 <ahmetcemsaydam@gmail.com>
2023-02-12 10:39:53 +0000

    added a solution to the Mini Max Sum Challenge
    added a solution to the Mini Max Sum Challenge in Java .

---
+++ Mini_Max_Sum.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        long sum = 0;
+        arr.sort(null);
+        for(Integer in : arr) {
+            sum += in;
+        }
+        System.out.println((sum - arr.get(arr.size()-1)) + " " + (sum  - arr.get(0)));
+    }
+}
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        Result.miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+}
