22d60e414b3ee5ed4a9f9d3f1db23bb216dcd464
asayda01 <ahmetcemsaydam@gmail.com>
2023-02-24 10:02:26 +0000

    added a solution to the Between Two Sets Challenge in JavaScript .
    added a solution to the Between Two Sets Challenge in JavaScript .

---
+++ Between_Two_Sets.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+function getTotalX(a, b) {
+    // Write your code here
+    const arr = [];
+    while(arr.length < b[0]) arr.push(arr.length + 1)
+    
+    let factors = 0;
+    arr.forEach((e) => {
+      const cond1 = a.every((el) => e % el === 0);
+      const cond2 = b.every((el) => el % e === 0);
+      if(cond1 && cond2) factors++;
+    })
+    
+    return factors;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const n = parseInt(firstMultipleInput[0], 10);
+
+    const m = parseInt(firstMultipleInput[1], 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const brr = readLine().replace(/\s+$/g, '').split(' ').map(brrTemp => parseInt(brrTemp, 10));
+
+    const total = getTotalX(arr, brr);
+
+    ws.write(total + '\n');
+
+    ws.end();
+}
