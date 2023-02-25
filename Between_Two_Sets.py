870dbb6a909e0217504e9621d2b9164d91c24d45
asayda01 <ahmetcemsaydam@gmail.com>
2023-02-25 11:23:01 +0000

    added a solution to the Between Two Sets Challenge in Python .
    added a solution to the Between Two Sets Challenge in Python .
    
    added a solution to the Between Two Sets Challenge in Python .

---
+++ Between_Two_Sets.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(setA, setB):
+    
+
+
+    maxA = max(setA)
+    minB = min(setB)
+    count = 0
+    for num in range(maxA, minB + 1):
+        left = all([num % numA == 0 for numA in setA])
+        right = all([numB % num == 0 for numB in setB])
+        count += left*right
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
