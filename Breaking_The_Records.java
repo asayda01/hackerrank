d8217e2ec9913d504fe66dcb55b3f8d91ccf4ade
asayda01 <ahmetcemsaydam@gmail.com>
2023-03-01 14:05:30 +0000

    added a solution the Breaking The Records Challenge
    added a solution the Breaking The Records Challenge in Java .
    
    added a solution the Breaking The Records Challenge in Java .

---
+++ Breaking_The_Records.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'breakingRecords' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts INTEGER_ARRAY scores as parameter.
+     */
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+        
+        int max = scores.get(0);
+        int min = scores.get(0);
+        int countMin = 0;
+        int countMax = 0;
+        for(int i = 1; i < scores.size(); i++){
+            if( max < scores.get(i)){
+                max = scores.get(i);
+                countMax++;
+            }
+            if( min > scores.get(i)){
+                min = scores.get(i);
+                countMin++;
+            }
+        }
+        return new ArrayList<Integer>(Arrays.asList(countMax, countMin));
+
+    }
+
+}
+
+public class Breaking_The_Records {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> scores = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        List<Integer> result = Result.breakingRecords(scores);
+
+        bufferedWriter.write(
+            result.stream()
+                .map(Object::toString)
+                .collect(joining(" "))
+            + "\n"
+        );
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
