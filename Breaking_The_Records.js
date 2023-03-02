dfc6f395c77529b898bc967261c06a9158c36ab1
asayda01 <ahmetcemsaydam@gmail.com>
2023-03-02 17:50:56 +0000

    added a solution to the Breaking The Records Challenge
    added a solution to the Breaking The Records Challenge in JavaScript .
    
    added a solution to the Breaking The Records Challenge in JavaScript .

---
+++ Breaking_The_Records.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'breakingRecords' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY scores as parameter.
+ */
+
+function breakingRecords(scores) {
+    let min = scores[0];
+    let max = scores[0];
+    let minCounter = 0;
+    let maxCounter = 0;
+    scores.forEach(value=>{
+        if(min>value){
+            minCounter++;
+            min = value;
+        }
+        if(max<value){
+            maxCounter++;
+            max = value;
+        }
+    })
+    return [maxCounter,minCounter];
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const scores = readLine().replace(/\s+$/g, '').split(' ').map(scoresTemp => parseInt(scoresTemp, 10));
+
+    const result = breakingRecords(scores);
+
+    ws.write(result.join(' ') + '\n');
+
+    ws.end();
+}
