49534ee1dcd04a3eae675e84e36b4faab2428cd1
asayda01 <ahmetcemsaydam@gmail.com>
2023-03-09 16:13:39 +0000

    added a solution the Subarray Division Challenge
    added a solution the Subarray Division Challenge in TypeScript .
    
    added a solution the Subarray Division Challenge in TypeScript .

---
+++ Subarray_Division.ts
@@ -0,0 +1,65 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'birthday' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY s
+ *  2. INTEGER d
+ *  3. INTEGER m
+ */
+
+function birthday(s: number[], d: number, m: number): number {
+    let count:number=0,sum:number=0
+    for(let i:number=0 ; i<s.length; i++){
+        for(let j:number = i ; j<m+i; j++){ sum+=s[j] }
+        if (sum === d) { count++ };
+        sum=0
+    };
+    return count;
+
+};
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const n: number = parseInt(readLine().trim(), 10);
+
+    const s: number[] = readLine().replace(/\s+$/g, '').split(' ').map(sTemp => parseInt(sTemp, 10));
+
+    const firstMultipleInput: string[] = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d: number = parseInt(firstMultipleInput[0], 10);
+
+    const m: number = parseInt(firstMultipleInput[1], 10);
+
+    const result: number = birthday(s, d, m);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
