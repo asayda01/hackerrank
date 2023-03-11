977e97f02584a5be8bb718b79a6514331e4f5756
asayda01 <ahmetcemsaydam@gmail.com>
2023-03-11 10:06:17 +0000

    added a solution to the Day Of The Programmer Challenge
    added a solution to the Day Of The Programmer Challenge in Java .
    
    added a solution to the Day Of The Programmer Challenge in Java .

---
+++ Day_Of_The_Programmer.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'dayOfProgrammer' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER year as parameter.
+     */
+
+    public static String dayOfProgrammer(int year) {
+        
+        if(year==1918){return "26.09."+year;}
+        if(year<1918){return (year%4==0) ? "12.09."+year : "13.09."+year;}
+        if(year>1918){return (year%400==0 || year%4==0 && year%100!=0) 
+        ? "12.09."+year : "13.09."+year;}
+        return "";
+
+    }
+
+}
+
+public class Day_Of_The_Programmer {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int year = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.dayOfProgrammer(year);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
