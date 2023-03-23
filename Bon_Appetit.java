a91fe27a3d5ab96109c3cdfbe7a06ba943908d19
asayda01 <ahmetcemsaydam@gmail.com>
2023-03-23 12:07:35 +0000

    added a solution the Bon Appetit Challenge
    added a solution the Bon Appetit Challenge in Java .
    
    added a solution the Bon Appetit Challenge in Java .

---
+++ Bon_Appetit.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'bonAppetit' function below.
+     *
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY bill
+     *  2. INTEGER k
+     *  3. INTEGER b
+     */
+
+    public static void bonAppetit(List<Integer> bill, int k, int b) {
+        int sum=0;
+        for(int i=0;i<bill.size();i++){
+            if(i==k) continue;
+            else{
+                sum+=bill.get(i);
+            }
+        }
+        
+        int sumActual = sum/2;
+        int minus=b-sumActual;
+        
+        if(sumActual==b) System.out.println("Bon Appetit");
+        else System.out.println(minus);
+        
+        }
+    
+    }
+
+public class Bon_Appetit {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int n = Integer.parseInt(firstMultipleInput[0]);
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        List<Integer> bill = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int b = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.bonAppetit(bill, k, b);
+
+        bufferedReader.close();
+    }
+}
