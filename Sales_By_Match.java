eeeff73c827db526745b740a4fb5eda05539eeef
asayda01 <ahmetcemsaydam@gmail.com>
2023-03-29 10:37:10 +0100

    added a solution the Sales By Match Challenge
    added a solution the Sales By Match Challenge in Java .
    
    added a solution the Sales By Match Challenge in Java .

---
+++ Sales_By_Match.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<Integer> ar) {
+        
+        List<Integer> distinct_ar = ar.stream().distinct().collect(Collectors.toList());
+        int count=0;
+        for (int i: distinct_ar) {
+            count += ar.stream().filter(j -> j==i).count()/2;
+        }
+        return count;
+    }
+
+
+}
+
+public class Sales_By_Match {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> ar = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
