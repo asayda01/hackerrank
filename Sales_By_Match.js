4725688ef1e8817d6797c1cc27b5539d843ec33a
asayda01 <ahmetcemsaydam@gmail.com>
2023-03-30 12:27:09 +0100

    added a solution the Sales By Match Challenge
    added a solution the Sales By Match Challenge in JavaScript .
    
    added a solution the Sales By Match Challenge in JavaScript .

---
+++ Sales_By_Match.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'sockMerchant' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER_ARRAY ar
+ */
+
+function sockMerchant(n, ar) {
+    
+    let pairCount = 0;
+    for (let i = 0; i < n; i++) {
+        if (ar[i] != -1) {
+            let indexOfPair = ar.indexOf(ar[i], i + 1);
+            if (indexOfPair > 0) {
+                pairCount++;
+                ar[indexOfPair] = -1;
+                ar[i] = -1;
+            }
+        }
+    }
+    return pairCount;
+
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const ar = readLine().replace(/\s+$/g, '').split(' ').map(arTemp => parseInt(arTemp, 10));
+
+    const result = sockMerchant(n, ar);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
