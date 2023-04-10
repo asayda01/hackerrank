31d2cd14016fbf6706bf0a3c36d2bad95dfeaff7
asayda01 <ahmetcemsaydam@gmail.com>
2023-04-10 09:40:52 +0100

    added a solution the Angry Professor Challenge
    added a solution the Angry Professor Challenge in TypeScript .
    
    added a solution the Angry Professor Challenge in TypeScript .

---
+++ Angry_Professor.ts
@@ -0,0 +1,60 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'angryProfessor' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER k
+ *  2. INTEGER_ARRAY a
+ */
+
+function angryProfessor(k: number, a: number[]): string {
+    return a.filter((num) => num<=0).length >= k ? "NO" : "YES";
+
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const t: number = parseInt(readLine().trim(), 10);
+
+    for (let tItr: number = 0; tItr < t; tItr++) {
+        const firstMultipleInput: string[] = readLine().replace(/\s+$/g, '').split(' ');
+
+        const n: number = parseInt(firstMultipleInput[0], 10);
+
+        const k: number = parseInt(firstMultipleInput[1], 10);
+
+        const a: number[] = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+        const result: string = angryProfessor(k, a);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
