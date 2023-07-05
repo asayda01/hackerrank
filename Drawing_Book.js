28b68877efff9f56f6cc5d3cba293e7a52e5c48c
asayda01 <ahmetcemsaydam@gmail.com>
2023-07-05 11:24:25 +0100

    added the Drawing Book exercise
    added the Drawing Book exercise and its solution in JavaScript .
    
    added the Drawing Book exercise and its solution in JavaScript .

---
+++ Drawing_Book.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+function pageCount(n, p) {
+    
+
+    return Math.floor(p/2) < Math.floor(n/2)-Math.floor(p/2) ?
+            Math.floor(p/2) : Math.floor(n/2)-Math.floor(p/2);
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const p = parseInt(readLine().trim(), 10);
+
+    const result = pageCount(n, p);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
