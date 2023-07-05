0c8201d8d4fb334c992b7d37d8c1fa01e407c006
asayda01 <ahmetcemsaydam@gmail.com>
2023-07-05 11:24:43 +0100

    added the Drawing Book exercise
    added the Drawing Book exercise and its solution in TypeScript .
    
    added the Drawing Book exercise and its solution in TypeScript .

---
+++ Drawing_Book.ts
@@ -0,0 +1,54 @@
+'use strict';
+
+import { WriteStream, createWriteStream } from "fs";
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+function pageCount(n: number, p: number): number {
+    
+    return Math.floor(p/2) < Math.floor(n/2)-Math.floor(p/2) ?
+        Math.floor(p/2) : Math.floor(n/2)-Math.floor(p/2);
+
+}
+
+function main() {
+    const ws: WriteStream = createWriteStream(process.env['OUTPUT_PATH']);
+
+    const n: number = parseInt(readLine().trim(), 10);
+
+    const p: number = parseInt(readLine().trim(), 10);
+
+    const result: number = pageCount(n, p);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
