ffc7a4ee4bf34a91aebf878958de22c0a39ccf11
asayda01 <ahmetcemsaydam@gmail.com>
2023-07-07 09:57:10 +0100

    added the Designer PDF Viewer exercise
    added the Designer PDF Viewer exercise and its solution in Java .
    
    added the Designer PDF Viewer exercise and its solution in Java .

---
+++ Designer_PDF_Viewer.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'designerPdfViewer' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY h
+     *  2. STRING word
+     */
+
+    public static int designerPdfViewer(List<Integer> h, String word) {
+        
+        char arr[] = word.toCharArray();
+        HashMap <Character,Integer> map = new HashMap ();
+       
+        for(int i = 0,j=0 ; i < arr.length&&j<arr.length;i++,j++){
+            int asci = (int)arr[i];
+            map.put(arr[j],h.get(asci-97));
+        
+        }
+        int max = Collections.max(map.values());
+        return max*word.length();
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        List<Integer> h = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        String word = bufferedReader.readLine();
+
+        int result = Result.designerPdfViewer(h, word);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
