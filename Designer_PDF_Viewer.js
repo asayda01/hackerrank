9eb43751f912764d52e95b2a2c150cbc70a16731
asayda01 <ahmetcemsaydam@gmail.com>
2023-07-08 10:35:39 +0100

    added the Designer PDF Viewer exercise
    added the Designer PDF Viewer exercise and its solution in JavaScript .
    
    added the Designer PDF Viewer exercise and its solution in JavaScript .

---
+++ Designer_PDF_Viewer.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'designerPdfViewer' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY h
+ *  2. STRING word
+ */
+
+function designerPdfViewer(h, word) {
+    
+  let maxHigh = 0;
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  for (let i = 0; i < word.length; i++) {
+    let letterIndex = alphabet.indexOf(word[i]);
+    if (h[letterIndex] > maxHigh) {
+      maxHigh = h[letterIndex];
+    }
+  }
+  return maxHigh * word.length;
+        
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const h = readLine().replace(/\s+$/g, '').split(' ').map(hTemp => parseInt(hTemp, 10));
+
+    const word = readLine();
+
+    const result = designerPdfViewer(h, word);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
