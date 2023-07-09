77226f6b5d5f75b9178832758e072d03dfe52a8d
asayda01 <ahmetcemsaydam@gmail.com>
2023-07-09 08:44:55 +0100

    added the Utopian Tree exercisse
    added the Utopian Tree exercise and its solution in Python .
    
    added the Utopian Tree exercise and its solution in Python .

---
+++ Utopian_Tree.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'utopianTree' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def utopianTree(n):
+    
+    if n < 3:
+        return n + 1
+    if n % 2 == 0:
+        return (utopianTree(n - 2) * 2) + 1
+    else:
+        return (utopianTree(n - 2) + 1) * 2
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
