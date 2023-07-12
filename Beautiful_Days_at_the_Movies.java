2923a9888abc923c6bb4297ded758ba2d319df1e
asayda01 <ahmetcemsaydam@gmail.com>
2023-07-12 10:27:56 +0100

    added the Beautiful Days At The Movies exercise
    added the Beautiful Days At The Movies exercise and its solution in Java .
    
    added the Beautiful Days At The Movies exercise and its solution in Java .

---
+++ Beautiful_Days_at_the_Movies.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'beautifulDays' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER i
+     *  2. INTEGER j
+     *  3. INTEGER k
+     */
+
+    public static int beautifulDays(int i, int j, int k) {
+        
+        int total = 0;
+        for (int l = i; l <= j; l++) {
+            String s = String.valueOf(l);
+            StringBuffer sbr = new StringBuffer(s);
+            sbr.reverse();
+            int reversed = Integer.valueOf(sbr.toString());
+            if (Math.abs(reversed - l) % k == 0)
+                total++;
+        }
+        return total;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int i = Integer.parseInt(firstMultipleInput[0]);
+
+        int j = Integer.parseInt(firstMultipleInput[1]);
+
+        int k = Integer.parseInt(firstMultipleInput[2]);
+
+        int result = Result.beautifulDays(i, j, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
