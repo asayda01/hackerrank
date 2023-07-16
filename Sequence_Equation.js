08d3befe32210b270889292778c3bc01c34e461d
asayda01 <ahmetcemsaydam@gmail.com>
2023-07-16 10:43:34 +0100

    added the Sequence Equation exercise
    added the Sequence Equation exercise and its solution in JavaScript .
    
    added the Sequence Equation exercise and its solution in JavaScript .

---
+++ Sequence_Equation.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'permutationEquation' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY p as parameter.
+ */
+
+function permutationEquation(p) {
+    
+    let y = [];
+    
+    p.forEach((item, i) => y[i] = p.indexOf(p.indexOf(i+1) + 1) + 1);
+    
+    return y;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const p = readLine().replace(/\s+$/g, '').split(' ').map(pTemp => parseInt(pTemp, 10));
+
+    const result = permutationEquation(p);
+
+    ws.write(result.join('\n') + '\n');
+
+    ws.end();
+}
