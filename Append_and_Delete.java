5a31d4113647805d286cea9ac30b1dee8c03a6c5
asayda01 <ahmetcemsaydam@gmail.com>
2023-07-19 09:10:38 +0100

    added the Append and Delete exercise
    added the Append and Delete exercise and its solution in Java .
    
    added the Append and Delete exercise and its solution in Java .

---
+++ Append_and_Delete.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'appendAndDelete' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. STRING t
+     *  3. INTEGER k
+     */
+
+    public static String appendAndDelete(String s, String t, int k) {
+        
+        if(k >= t.length() + s.length()) return "Yes";
+        int same = Math.min(t.length(), s.length());
+        for(int i = 0; i< Math.min(t.length(), s.length()); i++){
+            if(s.charAt(i) != t.charAt(i)){
+                same = i;
+                break;
+            }
+        }
+        k -= (s.length() - same);
+        k -= (t.length() - same);
+        
+        return (k >= 0 && k%2 == 0)? "Yes":"No";
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        String t = bufferedReader.readLine();
+
+        int k = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String result = Result.appendAndDelete(s, t, k);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
