027e8204bf66d91a10cc6ed6e56deca73f4737ff
asayda01 <ahmetcemsaydam@gmail.com>
2023-08-08 09:42:39 +0100

    added the Equalize the Array exercise
    added the Equalize the Array exercise and its solution in Java .
    
    added the Equalize the Array exercise and its solution in Java .

---
+++ Equalize_the_Array.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'equalizeArray' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int equalizeArray(List<Integer> arr) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int max = 0;
+        for(int x : arr){
+            hm.put(x, hm.getOrDefault(x, 0)+1 );
+            if(max<hm.get(x)){
+                max = hm.get(x);
+            }
+        }
+        return arr.size()-max;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.equalizeArray(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
