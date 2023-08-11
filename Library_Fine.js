fb569360b7d4da676f590093d495fb885baebdb5
asayda01 <ahmetcemsaydam@gmail.com>
2023-08-11 11:50:10 +0100

    added the Library Fine exercise
    added the Library Fine exercise and its solution in JavaScript .
    
    added the Library Fine exercise and its solution in JavaScript .

---
+++ Library_Fine.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'libraryFine' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER d1
+ *  2. INTEGER m1
+ *  3. INTEGER y1
+ *  4. INTEGER d2
+ *  5. INTEGER m2
+ *  6. INTEGER y2
+ */
+
+function libraryFine(d1, m1, y1, d2, m2, y2) {
+    
+    if (y1 > y2) {
+        return 10000
+    } else if (m1 > m2 && y1 >= y2) {
+        return 500 * (m1 - m2)
+    } else if (d1 > d2 && m1 >= m2 && y1 >= y2) {
+        return 15 * (d1 - d2)
+    } else {
+        return 0
+    }
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d1 = parseInt(firstMultipleInput[0], 10);
+
+    const m1 = parseInt(firstMultipleInput[1], 10);
+
+    const y1 = parseInt(firstMultipleInput[2], 10);
+
+    const secondMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const d2 = parseInt(secondMultipleInput[0], 10);
+
+    const m2 = parseInt(secondMultipleInput[1], 10);
+
+    const y2 = parseInt(secondMultipleInput[2], 10);
+
+    const result = libraryFine(d1, m1, y1, d2, m2, y2);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
