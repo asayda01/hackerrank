63d9178dce8602321d559963c741ecf09f28af84
asayda01 <ahmetcemsaydam@gmail.com>
2023-08-13 13:59:23 +0100

    added the Jumping On The Clouds Revisited exercise
    added the Jumping On The Clouds Revisited exercise and its solution in Java .
    
    added the Jumping On The Clouds Revisited exercise and its solution in Java .

---
+++ Jumping_on_the_Clouds_Revisited.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the jumpingOnClouds function below.
+    static int jumpingOnClouds(int[] c, int k) {
+        
+        int sum = c[0] == 1 ? 3:1;
+        if(c.length==k)
+        return 100 -sum;
+        int ct= k;
+        while(ct!=0){
+            if(c[ct]==0){
+                sum +=1;
+            }
+            else
+            sum +=3;
+            ct = ct+k;
+            if(ct==c.length){
+                ct =0;
+            }
+            if(ct>c.length){
+                ct = ct - c.length;
+            }
+        }
+        return 100 -sum;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] c = new int[n];
+
+        String[] cItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int cItem = Integer.parseInt(cItems[i]);
+            c[i] = cItem;
+        }
+
+        int result = jumpingOnClouds(c, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
