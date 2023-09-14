d42505578f0788d40f24310d9b17d56c251c4f4a
asayda01 <ahmetcemsaydam@gmail.com>
2023-09-14 09:58:09 +0100

    added the Minimum Distances exercise
    added the Minimum Distances exercise and it solution in Java .
    
    added the Minimum Distances exercise and it solution in Java .

---
+++ Minimum_Distances.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumDistances' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int minimumDistances(List<Integer> a) {
+    int result = -1;
+    for (int num : a) {
+        int pos1 =a.indexOf(num);
+        int pos2 =a.lastIndexOf(num);
+
+        if (pos1 != pos2) {
+            int dist = pos2 - pos1;
+            if ( (result == -1) || (result > dist) ) {
+                result = dist;
+            }
+        }
+    } 
+        return result;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.minimumDistances(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
