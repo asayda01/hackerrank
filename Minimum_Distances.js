820824cf3435268207d1c6d6b3151f6f4c82db7b
asayda01 <ahmetcemsaydam@gmail.com>
2023-09-15 10:18:21 +0100

    added the Minimum Distances exercise
    added the Minimum Distances exercise and its solution in JavaScript .
    
    added the Minimum Distances exercise and its solution in JavaScript .

---
+++ Minimum_Distances.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'minimumDistances' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function minimumDistances(a) {
+    
+    let arr = [];
+    for(let i = 0; i < a.length; i++) {
+        for(let j = i+1; j < a.length; j++) {
+            if(a[i] == a[j]) {
+                arr.push(Math.abs(i - j));
+            }
+        }
+    }
+    
+    return arr.length ? Math.min(...arr) : -1;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = minimumDistances(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
